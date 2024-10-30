ce9c647ae84b57a5c8f17eafef1aa4d15e4e2f81
aadarsh2305 <aadarshlohari91111@gmail.com>
2024-10-30 12:41:06 +0530

    Configured whole tailwind with Spring

---
+++ src/main/java/com/scm/controllers/PageController.java
@@ -0,0 +1,22 @@
+package com.scm.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("name", "Substring Tech");
+		model.addAttribute("YT", "https://chatgpt.com/");
+		model.addAttribute("Github", "https://github.com/aadarsh2305");
+		return "home";
+	}
+
+	@RequestMapping("/about")
+	public String about(){
+		return "home";
+	}
+}
