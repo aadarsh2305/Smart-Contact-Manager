9cc1b0082f5b2d0b8cdafab4b992bd5c36c49d09
aadarsh2305 <aadarshlohari91111@gmail.com>
2024-11-02 18:15:35 +0530

    two more routes added for testing

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -1,5 +1,6 @@
 package com.scm.controllers;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,5 +14,17 @@ public class PageController {
 		model.addAttribute("YT", "https://chatgpt.com/");
 		model.addAttribute("Github", "https://github.com/aadarsh2305");
 		return "home";
+	}
+
+	@RequestMapping("/about")
+	public String about(Model model){
+		System.out.println("About page loaded");
+		return "about";
+	}
+	
+	@RequestMapping("/services")
+	public String services(Model model){
+		System.out.println("Services page loaded");
+		return "service";
 	}
 }
