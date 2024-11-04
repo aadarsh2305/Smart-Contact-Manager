aba29be4ac5d7ed1c7c8f69c9a3eaf6a49da36fb
aadarsh2305 <aadarshlohari91111@gmail.com>
2024-11-04 19:39:17 +0530

    Register page UI created fully responsive using tailwind and flowbite

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,48 +1,46 @@
-console.log("script loaded");
+console.log("javascript loaded");
 
 let currenTheme = getTheme();
 
-document.addEventListener('DOMContentLoaded',()=>{
-    changeTheme();
+document.addEventListener("DOMContentLoaded", () => {
+  changeTheme();
 });
-
 
 // todo theme
 function changeTheme() {
-    // set to webpage
-    changePageTheme(currenTheme, currenTheme);
+  // set to webpage
+  changePageTheme(currenTheme, currenTheme);
 
-    // set the listener to change theme button
-    const changeThemeButton = document.querySelector("#theme_change_button");
+  // set the listener to change theme button
+  const changeThemeButton = document.querySelector("#theme_change_button");
 
-    // Initial button text
-    changeThemeButton.querySelector('span').textContent = currenTheme === "light" ? "Dark" : "Light";
+  // Initial button text
+  changeThemeButton.querySelector("span").textContent =
+    currenTheme === "light" ? "Dark" : "Light";
 
-    changeThemeButton.addEventListener("click", () => {
-        console.log("Button clicked");
-        const oldTheme = currenTheme;
+  changeThemeButton.addEventListener("click", () => {
+    console.log("Button clicked");
+    const oldTheme = currenTheme;
 
-        // Toggle the theme
-        currenTheme = currenTheme === "dark" ? "light" : "dark";
+    // Toggle the theme
+    currenTheme = currenTheme === "dark" ? "light" : "dark";
 
-        changePageTheme(currenTheme, oldTheme);
-
-    });
+    changePageTheme(currenTheme, oldTheme);
+  });
 }
 
 // change current page theme
-function changePageTheme(theme, oldTheme){
-    
-    // Update in local storage
-    setTheme(currenTheme);
-        
-    // Remove old theme and add new theme
-    document.querySelector('html').classList.remove(oldTheme);  
-    document.querySelector('html').classList.add(currenTheme);
-    
-    // Update button text after theme change
-    document.querySelector('#theme_change_button')
-    .querySelector('span').textContent = theme === "light" ? "Dark" : "Light";
+function changePageTheme(theme, oldTheme) {
+  // Update in local storage
+  setTheme(currenTheme);
+  // Remove old theme and add new theme
+  document.querySelector("html").classList.remove(oldTheme);
+  document.querySelector("html").classList.add(currenTheme);
+
+  // Update button text after theme change
+  document
+    .querySelector("#theme_change_button")
+    .querySelector("span").textContent = theme === "light" ? "Dark" : "Light";
 }
 
 //Set theme to local storage
