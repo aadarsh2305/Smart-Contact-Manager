6eedd028f3e37f41d2f54074119f52848a6da110
aadarsh2305 <aadarshlohari91111@gmail.com>
2024-11-18 13:04:12 +0530

    By default home page will trigger from controller

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -24,6 +24,11 @@ public class PageController {
 
 	@Autowired
 	private UserService userService;
+
+	@GetMapping("/")
+	public String index(){
+		return "redirect:/home";
+	}
 	
 	@RequestMapping("/home")
 	public String home(Model model) {
