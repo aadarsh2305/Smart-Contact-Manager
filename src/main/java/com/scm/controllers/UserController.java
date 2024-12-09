247149bd28187c7faa6fe207942b500d98b9c8f7
Aadarsh <Aadarsh.Gupta@Prolifics.com>
2024-12-09 16:44:33 +0530

    Managed login user an got the user email of logged in user

--- src/main/java/com/scm/controllers/UserController.java
+++ src/main/java/com/scm/controllers/UserController.java
@@ -1,17 +1,23 @@
 package com.scm.controllers;
 
+import java.security.Principal;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import com.scm.exception.Helper;
 
 
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
 
     // Dashboard page: Support both GET and POST
     @RequestMapping(value = "/dashboard", method = {RequestMethod.GET, RequestMethod.POST})
@@ -21,7 +27,11 @@ public class UserController {
 
     // User profile page
     @RequestMapping(value = "/profile", method = RequestMethod.GET)
-    public String userProfile() {
+    public String userProfile(Authentication authentication) {
+        String userName = Helper.getEmailOfLoggedInUser(authentication);
+        logger.info("Logged in user got it from princeple class: "+ userName);
+
+        // Database se data fetch karenge
         return "user/profile";
     }
     
