c25c6b8bb87130dd63181e6dcd00850c50db8a61
Aadarsh <Aadarsh.Gupta@Prolifics.com>
2025-01-22 19:44:03 +0530

    added add contact form in user

--- src/main/java/com/scm/entities/Contact.java
+++ src/main/java/com/scm/entities/Contact.java
@@ -11,9 +11,17 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Contact {
 
     @Id
